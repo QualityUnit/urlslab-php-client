6ff1598dd1adabf95e115385a5225723ebe779da
QualityUnit <yboroumand@qualityuit.com>
2023-03-09 07:27:07 +0000

    urlslab php client: update to version 0.0.10

--- git_push.sh
+++ git_push.sh
@@ -9,12 +9,12 @@ release_note=$3
 git_repo_base_url=$4
 
 if [ "$git_user_id" = "" ]; then
-    git_user_id="urlslab-php-client"
+    git_user_id="urlslab"
     echo "[INFO] No command line input provided. Set \$git_user_id to $git_user_id"
 fi
 
 if [ "$git_repo_id" = "" ]; then
-    git_repo_id="urlslab"
+    git_repo_id="urlslab-php-sdk"
     echo "[INFO] No command line input provided. Set \$git_repo_id to $git_repo_id"
 fi
 
